96b9044373422908ffe42648fc3a94ca4c64dac1
Armandsikk03 <armand.sikk@tktk.ee>
2024-11-03 21:32:14 +0200

    add week3ifandswitch code

---
+++ Week3ifandswitch/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("enter a number");
+int username = Convert.ToInt32(Console.ReadLine());
+
+//kas kasutaja arv on paaris või paaritu
+
+int result = username % 2;
+
+Console.WriteLine(result);
+
+if (result != 0) // != ei ole
+{
+    Console.WriteLine("user number is odd");
+}
+else
+{
+    Console.WriteLine("user number is even");
+}
